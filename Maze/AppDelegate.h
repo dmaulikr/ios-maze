c2ef07c8201417ff98135c57371acffcff0ac297
Abhijit Joshi <joshi1974@gmail.com>
2014-05-21 01:45:44 -0400

    Initial Commit

---
+++ Maze/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Maze
+//
+//  Created by Abhijit Joshi on 5/21/14.
+//  Copyright (c) 2014 Misha software solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Maze/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Maze
+//
+//  Created by Abhijit Joshi on 5/21/14.
+//  Copyright (c) 2014 Misha software solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
